5114704b2c19b8f5ddd4405fecb74fe084e45019
Tim <mail@timlabahn.de>
2023-11-10 08:25:52 +0000

    delete page fixed

--- resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
-import DangerButton from '@/Components/Defaults/DangerButton.vue';
-import InputError from '@/Components/Defaults/InputError.vue';
-import InputLabel from '@/Components/Defaults/InputLabel.vue';
-import Modal from '@/Components/Defaults/Modal.vue';
-import SecondaryButton from '@/Components/Defaults/SecondaryButton.vue';
-import TextInput from '@/Components/Defaults/TextInput.vue';
 import { useForm } from '@inertiajs/vue3';
+import { Button, Modal, PasswordInput } from 'custom-mbd-components';
 import { nextTick, ref } from 'vue';
 
 const confirmingUserDeletion = ref(false);
@@ -40,49 +35,40 @@ const closeModal = () => {
 </script>
 
 <template>
-    <section class="space-y-6">
-        <header>
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Delete Account</h2>
+    <Modal
+        :title="'Sind sie sicher das sie ihren Account löschen wollen?'"
+        :show="confirmingUserDeletion"
+        @close="closeModal"
+        :affirm="{
+            class: 'btn btn-danger',
+            text: 'Account Löschen',
+            action: () => deleteUser,
+        }"
+        :negative="{
+            class: 'btn btn-primary',
+            text: 'Abbrechen',
+            action: () => closeModal,
+        }"
+    >
+        <template #button>
+            <header>
+                <h2>Account Löschen</h2>
+                <p>Sobald ihr Account gelöscht ist, gehen all ihre daten unwiederruflich verloren.</p>
+            </header>
 
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download
-                any data or information that you wish to retain.
-            </p>
-        </header>
+            <Button @click="confirmUserDeletion" class="btn btn-danger">Delete Account</Button>
+        </template>
 
-        <DangerButton @click="confirmUserDeletion">Delete Account</DangerButton>
-
-        <Modal :show="confirmingUserDeletion" @close="closeModal">
-            <div class="p-6">
-                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Are you sure you want to delete your account?</h2>
-
-                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you
-                    would like to permanently delete your account.
-                </p>
-
-                <div class="mt-6">
-                    <InputLabel for="password" value="Password" class="sr-only" />
-
-                    <TextInput
-                        id="password"
-                        ref="passwordInput"
-                        v-model="form.password"
-                        type="password"
-                        class="mt-1 block w-3/4"
-                        placeholder="Password"
-                        @keyup.enter="deleteUser"
-                    />
-
-                    <InputError :message="form.errors.password" class="mt-2" />
-                </div>
-
-                <div class="mt-6 flex justify-end">
-                    <SecondaryButton @click="closeModal">Cancel</SecondaryButton>
-
-                    <DangerButton class="ml-3 btn btn-danger" @click="deleteUser">Delete Account</DangerButton>
-                </div>
-            </div>
-        </Modal>
-    </section>
+        <div class="p-6">
+            <p>Sobald ihr Account gelöscht ist, gehen all ihre daten unwiederruflich verloren.</p>
+            <PasswordInput
+                id="password"
+                ref="passwordInput"
+                v-model="form.password"
+                type="password"
+                placeholder="Password"
+                @keyup.enter="deleteUser"
+            />
+        </div>
+    </Modal>
 </template>
